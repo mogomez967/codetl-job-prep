3fcf5251d15cee260c109a60560c230aa140b065
Adam Hong <adam@travelbank.com>
2024-03-19 14:52:33 -0700

    feat(employee-benefits): take home project 1

---
+++ Employee Benefits/start_here.rb
@@ -0,0 +1,26 @@
+# Exercise: Employee Benefits Spending Analysis
+#
+# You are tasked with implementing a tool to help our company's benefits team manage and analyze employee benefits spending.
+# A JSON file has been provided containing detailed data on benefits spending by department for the current month.
+#
+# Your objectives are:
+# 1. Read the provided JSON file to analyze the data.
+# 2. Calculate the average total amount of benefits spending per user for each department.
+# 3. Determine which department has the highest and the lowest average spending per employee.
+#
+# (Extra) 4. Calculate which benefit has been spent on the most across the company for the data.
+#
+# Sample JSON structure:
+# {
+#   "departments": [
+#     {
+#       "name": "Engineering",
+#       "users": [
+#         {"id": 1, "spending": {"food": 120, "wellness": 150, "phone_wifi": 80, "transportation": 100, "health_insurance": 200}},
+#         ...
+#       ]
+#     },
+#     ...
+#   ]
+# }
+#
